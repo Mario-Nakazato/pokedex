06712a4bf6cfe81936d5eaddb1d03a30e5c7e861
Mario Nakazato <marionakazat@gmail.com>
2024-10-02 09:19:34 -0300

    Home page

--- pokedex/src/views/Home.vue
+++ pokedex/src/views/Home.vue
@@ -1,11 +1,26 @@
 <template>
-	<div>
-		<h1>Inicio</h1>
+	<div class="d-flex flex-column justify-content-center align-items-center vh-80"
+		style="background-color: #ff7029; box-shadow: 0 0 15px rgba(0, 0, 0, 0.8);">
+		<div class="part-one">108</div>
+		<div class="part-two">4 8 15 16 23 42</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
-<style scoped></style>
+<style scoped>
+.part-one {
+	font-size: 4rem;
+	font-weight: bold;
+	margin-bottom: 25px;
+	color: #FFD700;
+	text-shadow: 3px 3px 5px rgba(0, 0, 0, 0.7);
+}
+
+.part-two {
+	font-size: 2rem;
+	color: #FFFFFF;
+	text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+}
+</style>
